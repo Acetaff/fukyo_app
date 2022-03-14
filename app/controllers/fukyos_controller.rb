584f5552d0e37738572ab4e261b34dc3445fb984
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-14 18:00:56 +0900

    コントローラーのcreateアクションを編集

--- app/controllers/fukyos_controller.rb
+++ app/controllers/fukyos_controller.rb
@@ -8,6 +8,17 @@ class FukyosController < ApplicationController
   end
 
   def create
+    @fukyo = Fukyo.new(fukyo_params)
+    if @fukyo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def fukyo_params
+    params.require(:fukyo).permit(:title, :f_text, :f_url)
   end
 
 end
