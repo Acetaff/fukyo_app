ea1bd982ef53436e509e4e6c3c3554b935289d9f
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-14 15:34:05 +0900

    バリデーションを設定＋ビューファイルを微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :fukyo_sya, presence: true
+  validates :profile,   presence: true
 end
