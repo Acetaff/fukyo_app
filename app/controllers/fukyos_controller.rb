a5e1259a3fd15b2e5099d76a6506b07082198436
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-15 17:43:35 +0900

    ログインしていないユーザーのアクションを制限

--- app/controllers/fukyos_controller.rb
+++ app/controllers/fukyos_controller.rb
@@ -1,4 +1,5 @@
 class FukyosController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_fukyo, only: [:show, :edit, :update]
 
   def index 
