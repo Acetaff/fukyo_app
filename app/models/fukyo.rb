23d17523eded61d1de2d9e7762c99cc2db8e9924
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-15 14:48:40 +0900

    画像を保存〜表示させる記述を追加

--- app/models/fukyo.rb
+++ app/models/fukyo.rb
@@ -2,6 +2,7 @@ class Fukyo < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates  :title,  presence: true
-  validates  :f_text, presence: true
+  validates :title,  presence: true
+  validates :f_text, presence: true
+  validates :image,  presence: true
 end
