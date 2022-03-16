1d386781e9662c81c3d0c198acf9fdb5f441b698
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-16 12:29:30 +0900

    各モデルにアソシエーションを追記

--- app/models/fukyo.rb
+++ app/models/fukyo.rb
@@ -1,6 +1,7 @@
 class Fukyo < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_many :comments
 
   validates :title,  presence: true
   validates :f_text, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many  :fukyos
+  has_many  :comments
 
   validates :fukyo_sya, presence: true
   validates :profile,   presence: true
