c1a954ed977dd05e22788ff80c206ade8e8337c7
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-16 12:08:30 +0900

    コントローラーに保存の処理を設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(comment_params)
+    if @comment.save
+    redirect_to "/fukyos/#{@comment.fukyo.id}"
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:c_text).merge(user_id: current_user.id, fukyo_id: params[:fukyo_id])
+  end
 end
