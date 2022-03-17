e2ad02ddbe24609a960a964372a311fe6a403f3a
Acetaff <98571070+Acetaff@users.noreply.github.com>
2022-03-17 18:07:07 +0900

    各モデルのアソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many  :fukyos
   has_many  :comments
   has_many  :favorites
+  has_many  :relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many  :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many  :followings, through: :relationships, source: :followed
+  has_many  :followers, through: :reverse_of_relationships, source: :follower
 
   validates :fukyo_sya, presence: true
   validates :profile,   presence: true
